4e1f694efdbe318b54db208eef74e3b1efd5cf39
Bryan Gonzalez <bgonza868@gmail.com>
2018-10-09 23:04:01 -0400

    rearrange, new pester test

--- tests/Meraki.PSCLI.Tests.ps1
+++ tests/Meraki.PSCLI.Tests.ps1
@@ -1,5 +1,11 @@
+if(-not $PSScriptRoot){
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Definition -Parent
+}
+
+$psVersion = $PSVersionTable.PSVersion.Major
+
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
-$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*\*.psm1")
+$moduleRoot = Split-Path (Resolve-Path "$PSScriptRoot\*\*.psm1") 
 $moduleName = Split-Path $moduleRoot -Leaf
 
 Describe "General project validation: $moduleName" {
@@ -8,7 +14,7 @@ Describe "General project validation: $moduleName" {
 
     # TestCases are splatted to the script so we need hashtables
     $testCase = $scripts | Foreach-Object {@{file = $_}}         
-    It "Script <file> should be valid powershell" -TestCases $testCase {
+    It "Script <file> should be valid powershell (PS$psVersion)" -TestCases $testCase {
         param($file)
 
         $file.fullname | Should Exist
@@ -19,7 +25,7 @@ Describe "General project validation: $moduleName" {
         $errors.Count | Should Be 0
     }
 
-    It "Module '$moduleName' can import cleanly" {
+    It "Module '$moduleName' can import cleanly (PS$psVersion)" {
         {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
     }
 }
---
+++ tests/appveyor.tests.ps1
@@ -0,0 +1,52 @@
+param([switch]$Finalize)
+
+# Globals
+$psVersion = $PSVersionTable.PSVersion.Major
+$testFile = "TestResultsPS$psVersion.xml"
+$projectRoot = $ENV:APPVEYOR_BUILD_DIR
+
+Set-Location $projectRoot
+
+# Run tests for current version
+if (-not $Finalize) {
+    Write-Host "`n`tSTATUS: Testing with PowerShell $psVersion`n"
+
+    Import-Module Pester
+
+    Invoke-Pester -Path "$projectRoot\tests" -OutputFormat NUnitXml -OutputFile "$projectRoot\$testFile" -PassThru |
+        Export-Clixml -Path "$projectRoot\PesterResults$psVersion.xml"
+}
+
+# Finalizing
+else {
+    $allFiles = Get-ChildItem -Path $projectRoot\*Results*.xml | Select-Object -ExpandProperty FullName
+    Write-Host "`n`tSTATUS: Finalizing results`n"
+    Write-Host "Concatenating results:`n$($allFiles | Out-String)"
+
+    # Upload to appveyor
+    Get-ChildItem -Path "$projectRoot\TestResultsPS*.xml" | ForEach-Object {
+        $url = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+        $file = $_.FullName
+
+        Write-Host "UPLOADING: $file to $url"
+
+        (New-Object -TypeName System.Net.WebClient).UploadFile($url, $file)
+    }
+
+    # Log failures
+    $results = @(Get-ChildItem -Path "$projectRoot\PesterResults*.xml" | Import-Clixml)
+
+    $failureCount = $results | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+
+    if ($failureCount -gt 0) {
+        $failedItems = $results | Select-Object -ExpandProperty TestResult | Where-Object {$_.Passed -notlike $true}
+        
+        Write-Host "Failure Summary:`n"
+        $failedItems | ForEach-Object {
+            $test = $_
+            $test.Name = "It $($test.Name)"
+        } | Sort-Object Describe, Context, Name, Result| Format-List
+
+        throw "$failureCount tests failed."
+    }
+}
